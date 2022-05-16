f24eba5ad209b3cac40d0b6ff168b0046d2d174a
Renato Teixeira <rena2damas@gmail.com>
2022-05-16 12:35:52 +0200

    Configure quasar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import {createApp} from "vue"
+import {Quasar} from "quasar"
+
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css"
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css"
+
+// Import Quasar css
+import "quasar/src/css/index.sass"
 
 import App from "@/App.vue"
 
-createApp(App).mount("#app")
+const app = createApp(App)
+
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount("#app")
