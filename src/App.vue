6b68a919511415cafcb2b5be71ed1b81176f4107
Renato Teixeira <rena2damas@gmail.com>
2022-05-16 12:36:02 +0200

    Base structure

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,49 @@
 <script setup>
-import HelloWorld from "@/components/HelloWorld.vue"
-import TheWelcome from "@/components/TheWelcome.vue"
+// import Header from "@/components/Header.vue"
+import Home from "@/components/Home.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
+  <q-layout view="lhr lpr lfr" class="">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
+    <div class="row">
+      <q-header reveal :reveal-offset="0" :style="{
+        backgroundColor: 'transparent',
+        fontFamily: 'Reenie Beanie, cursive',
+        color: '#274C77'
+      }" class="col-sm-10 col-md-8 offset-sm-1 offset-md-2">
+        <q-toolbar>
+          <q-toolbar-title :style="{
+            fontWeight: 700,
+            fontSize: '1.2rem',
+          }">
+            Renato & Barbara
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome/>
-  </main>
+    <q-page-container>
+      <!--      <Home/>-->
+      <q-page>
+        <Home/>
+      </q-page>
+
+      <q-page-scroller position="bottom-right" :scroll-offset="150">
+        <q-btn fab icon="keyboard_arrow_up" color="accent"/>
+      </q-page-scroller>
+    </q-page-container>
+
+    <!--    <q-footer>-->
+    <!--      <q-toolbar>-->
+    <!--        <q-toolbar-title>-->
+    <!--          <div>Title</div>-->
+    <!--        </q-toolbar-title>-->
+    <!--      </q-toolbar>-->
+    <!--    </q-footer>-->
+
+  </q-layout>
 </template>
 
-<style lang="scss">
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
+<style scoped>
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<template>
+  <section class="q-py-xl q-px-md">
+    <div class="row full-height">
+      <div class="col-sm-10 col-md-8 offset-sm-1 offset-md-2">
+        <div class="row full-height">
+          <div class="col-6 relative-position">
+            <h1 class="text-white">We are getting married</h1>
+            <div class="absolute-bottom-left">
+
+              <h4 class="text-white">
+                <span class="material-icons text-white q-mr-sm">calendar_month</span>
+                04 March, 2023
+              </h4>
+              <h4 class="text-white q-pt-lg">
+                <span class="material-icons text-white q-mr-sm">pin_drop</span>
+                Quinta da Bichinha
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  background: url("../assets/home-bkg.jpeg") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 700px;
+
+  h1, h2, h3, h4 {
+    font-family: "Amatic SC", cursive;
+    font-weight: 700;
+    font-style: normal;
+  }
+}
+</style>
